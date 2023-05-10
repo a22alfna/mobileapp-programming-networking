6bd0bbee49f280f77ab57e58413feaf801012dfa
a22alfna <a22alfna@student.his.se>
2023-05-10 14:34:59 +0200

    Jag fixade MyViewHolder.java filen och lade till olika variabler och metoder

--- app/src/main/java/com/example/networking/MyViewHolder.java
+++ app/src/main/java/com/example/networking/MyViewHolder.java
@@ -1,4 +1,20 @@
 package com.example.networking;
 
-public class MyViewHolder {
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public TextView name;
+    public TextView size;
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+        name = itemView.findViewById(R.id.name);
+        size = itemView.findViewById(R.id.size);
+    }
+
 }
