943746c9fdb5b855b580d59309159fc7a1f92718
a22alfna <a22alfna@student.his.se>
2023-05-10 14:25:16 +0200

    Jag fixade MyAdapter.java filen och lade till de olika variabler och metoder som behövdes

--- app/src/main/java/com/example/networking/MyAdapter.java
+++ app/src/main/java/com/example/networking/MyAdapter.java
@@ -1,4 +1,36 @@
 package com.example.networking;
 
-public class MyAdapter {
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.view.ViewGroup;
+
+    import androidx.annotation.NonNull;
+    import androidx.recyclerview.widget.RecyclerView;
+
+    import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    private List<Mountain> Mountain;
+    public MyAdapter(List<Mountain> Mountain) {
+        this.Mountain = Mountain;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_mountain, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.name.setText(Mountain.get(position).getName());
+        holder.size.setText(String.valueOf(Mountain.get(position).getSize()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return Mountain.size();
+    }
+
 }
