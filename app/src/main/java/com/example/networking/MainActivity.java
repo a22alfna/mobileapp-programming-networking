9058c0b40c83c5dda1477bfcb0d79aa006d37597
a22alfna <a22alfna@student.his.se>
2023-05-10 12:18:11 +0200

    Jag fixade min MainActivity.java sida

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,33 +4,51 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
-    private final String JSON_FILE = "mountains.json";
 
-    private androidx.recyclerview.widget.RecyclerView RecyclerView;
+
+    private RecyclerView RecyclerView;
 
     private ArrayList<Mountain> Mountain;
 
     private MyAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        RecyclerView = findViewById(R.id.myRecycler_view);
+        Mountain = new ArrayList<Mountain>();
+        adapter = new MyAdapter(Mountain);
+        RecyclerView.setAdapter(adapter);
+        RecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        new JsonTask(this).execute(JSON_URL);
     }
 
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
     }
+        Log.d("tomten", "onPostExecute: " +json);
 
+    Gson gson = new Gson();
+    Type type = new TypeToken<ArrayList<Mountain>>() {}.getType();
+    ArrayList<Mountain> data = gson.fromJson(json, type);
+    Mountain.addAll(data);
+    adapter.notifyDataSetChanged();
 }
